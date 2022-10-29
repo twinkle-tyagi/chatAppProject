ae507a9948b14a8faf13f028fd1aee4e11e90384
twinkle-tyagi <twinkletyagi@niet.co.in>
2022-10-29 14:31:41 +0530

    signup

---
+++ util/database.js
@@ -0,0 +1,6 @@
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_ADMIN_ID, process.env.DB_ADMIN_PASSWORD, {
+    dialect: 'mysql',
+    host: process.env.DB_HOST
+})
